05bb3b5499b652cb34dc20bb0f6b0a7fbbea4913
akatomakhina <katomakhinaanastasiya@gmail.com>
2018-03-27 06:04:13 +0300

    Project with a string extension added.

---
+++ NET.S.2018.Katomakhina.05/StringRepresentation/StringRepresentationLogic.Tests/StringRepresentationConverterTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringRepresentationLogic.Tests
+{
+    [TestFixture]
+    public class StringRepresentationConverterTest
+    {
+        [Test]
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        [TestCase("11101101111011001100001010", 2, ExpectedResult = 62370570)]
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        [TestCase("10", 5, ExpectedResult = 5)]
+
+        public static long ToDecimalForm_Numbers_Result(string source, int systemIndex)
+            => source.ToDecimalForm(new StringRepresentationConverter.Notation(systemIndex));
+
+        [Test]
+        [TestCase("1000011101110101", ExpectedResult = 34677)]
+        public static long ToDecimalForm_Numbers_Result_WithDefaultBasis(string source)
+            => source.ToDecimalForm(new StringRepresentationConverter.Notation());
+
+        [Test]
+        [TestCase("1AeF101", 2)]
+        [TestCase("SA123", 2)]
+        public static void ArgumentException_ToDecimalFormArgumentException(string source, int systemIndex)
+            => Assert.Throws<ArgumentException>(() => source.ToDecimalForm(new StringRepresentationConverter.Notation()));
+
+        [Test]
+        [TestCase("11111111111111111111111111111111", 2)]
+        public static void OverflowException_ToDecimalFormOverflowException(string source, int systemIndex)
+            => Assert.Throws<OverflowException>(() => source.ToDecimalForm(new StringRepresentationConverter.Notation()));
+
+        [Test]
+        [TestCase("764241", 20)]
+        public static void ArgumentOutOfRangeException_ToDecimalFormArgumentException(string source, int systemIndex)
+            => Assert.Throws<ArgumentOutOfRangeException>(() => source.ToDecimalForm(new StringRepresentationConverter.Notation()));
+
+        [Test]
+        [TestCase(null, 2)]
+        public static void ArgumentNullException_ToDecimalFormOverflowException(string source, int systemIndex)
+            => Assert.Throws<ArgumentNullException>(() => source.ToDecimalForm(new StringRepresentationConverter.Notation()));
+
+    }
+}
---
+++ NET.S.2018.Katomakhina.05/StringRepresentation/StringRepresentationLogic/StringRepresentationConverter.cs
@@ -0,0 +1,138 @@
+﻿// <copyright file="StringRepresentationConverter.cs" company="Nastichka Enterprises">
+//     Copyright? I dont know what is it.
+// </copyright>
+// <author>Anastasiya Katomakhina</author>
+
+namespace StringRepresentationLogic
+{
+    using System;
+
+    /// <summary>
+    /// This class contains the implementation of the method of extending 
+    /// the output from the string representation 
+    /// of the whole positive four-byte number written in the p-ary number system, its decimal value.
+    /// </summary>
+    public static class StringRepresentationConverter
+    {
+        #region PublicMethod
+        /// <summary>
+        /// The public method contains a method implements an algorithm for obtaining 
+        /// from the string representation a whole positive four-byte number written 
+        /// in the p-ary number system, its decimal value.
+        /// </summary>
+        /// <param name="source">The received string containing the number.</param>
+        /// <param name="notation">An instance of a class that describes the desired number system.</param>
+        /// <returns>The method returns a decimal value of the number.</returns>
+        /// <exception cref="ArgumentNullException">Source is null.</exception>
+        public static int ToDecimalForm(this string source, Notation notation)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return ToDecimalForAlgorithm(source, notation);
+        }
+        #endregion
+
+        #region PrivateMethod
+        /// <summary>
+        /// The private method implements an algorithm for obtaining 
+        /// from the string representation a whole positive four-byte number written 
+        /// in the p-ary number system, its decimal value.
+        /// </summary>
+        /// <param name="source">The received string containing the number.</param>
+        /// <param name="notation">An instance of a class that describes the desired number system.</param>
+        /// <returns>The method returns a decimal value of the number.</returns>
+        private static int ToDecimalForAlgorithm(string source, Notation notation)
+        {
+            int index = notation.NotationProperty;
+
+            int result = 0;
+            char[] array = source.ToCharArray();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                int temp = notation.GetTheSymbolToDecimal(array[i]);
+
+                checked
+                {
+                    result += temp * (int)Math.Pow(index, array.Length - i - 1);
+                }
+            }
+
+            return result;
+        }
+        #endregion
+
+        #region InternalPublicСlass
+        /// <summary>
+        /// Internal class containing the abstraction of the number system.
+        /// </summary>
+        public class Notation
+        {
+            /// <summary>
+            /// A constant field describes a binary number system.
+            /// </summary>
+            private const int MinNumberSystem = 2;
+
+            /// <summary>
+            /// The constant field describes the hexadecimal number system. 
+            /// </summary>
+            private const int MaxNumberSystem = 16;
+
+            /// <summary>
+            /// A string with the characters that we will use for digit.
+            /// </summary>
+            private const string Alphabet = "0123456789ABCDEF";
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Notation"/> class with binary basis.
+            /// </summary>
+            public Notation()
+            {
+                this.NotationProperty = MinNumberSystem;
+            }
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Notation"/> class with received basis.
+            /// </summary>
+            /// <param name="basis">The received basis</param>
+            /// <exception cref="ArgumentOutOfRangeException">Basis is out 
+            /// of range of the notation borders</exception>
+            public Notation(int basis)
+            {
+                if (basis < MinNumberSystem || basis > MaxNumberSystem)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(basis));
+                }
+
+                this.NotationProperty = basis;
+            }
+
+            /// <summary>
+            /// Property for class Notation.
+            /// </summary>
+            public int NotationProperty { get; set; }
+
+            /// <summary>
+            /// Convertes received symbols to decimal.
+            /// </summary>
+            /// <param name="receivedСhar">The received char.</param>
+            /// <returns>Returns a character in decimal representation.</returns>
+            public int GetTheSymbolToDecimal(char receivedСhar)
+            {
+                int decimalNumber;
+                decimalNumber = Alphabet.IndexOf(char.ToUpper(receivedСhar));
+
+                if (decimalNumber >= this.NotationProperty)
+                {
+                    throw new ArgumentException(nameof(receivedСhar));
+                }
+
+                return decimalNumber;
+            }
+        }
+        #endregion
+    }
+}
