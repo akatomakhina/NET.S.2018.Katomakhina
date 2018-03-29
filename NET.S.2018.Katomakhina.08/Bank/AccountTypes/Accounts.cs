f2856ff06405bec515d0ddc591be766bef3bb1d5
akatomakhina <katomakhinaanastasiya@gmail.com>
2018-03-29 17:39:17 +0300

    Bank system added

---
+++ NET.S.2018.Katomakhina.08/Bank/AccountTypes/Accounts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BankSystem;
+
+namespace AccountTypes
+{
+    public class BaseAccount : AbstractAccount
+    {
+        protected override bool IsValidBalance(decimal value) => value >= 0;
+
+        protected override int CalculateBanefitPoints(decimal amount) => (int)Math.Round(amount * 0.01m + Balance * 0.01m);
+    }
+
+
+    public class SilverAccount : AbstractAccount
+    {
+        protected override bool IsValidBalance(decimal value) => value > -1000;
+
+        protected override int CalculateBanefitPoints(decimal amount) => (int)Math.Round(amount * 0.03m + Balance * 0.03m);
+    }
+
+    public class GoldAccount : AbstractAccount
+    {
+        protected override bool IsValidBalance(decimal value) => value > -10000;
+
+        protected override int CalculateBanefitPoints(decimal amount) => (int)Math.Round(amount * 0.1m + Balance * 0.1m);
+    }
+
+    public class PlatinumAccount : AbstractAccount
+    {
+        protected override bool IsValidBalance(decimal value) => value > -50000;
+
+        protected override int CalculateBanefitPoints(decimal amount) => (int)Math.Round(amount * 0.5m + Balance * 0.5m);
+    }
+}
---
+++ NET.S.2018.Katomakhina.08/Bank/BankSystem/AbstractAccount.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace BankSystem
+{
+    public abstract class AbstractAccount
+    {
+        private string accountNumber;
+        private string name;
+        private decimal balance;
+        private int bonusPoints;
+
+        public string AccountNumber
+        {
+            get => accountNumber;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    accountNumber = value;
+                }
+                else
+                {
+                    throw new ArgumentException(nameof(accountNumber));
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException(nameof(name));
+                }
+            }
+        }
+
+        public decimal Balance
+        {
+            get => balance;
+            private set
+            {
+                if (IsValidBalance(value))
+                {
+                    balance = value;
+                }
+                else
+                {
+                    throw new ArgumentException(nameof(balance));
+                }
+            }
+        }
+
+        public int BenefitPoints
+        {
+            get => bonusPoints;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException(nameof(amount));
+            }
+            Balance += amount;
+            bonusPoints += CalculateBanefitPoints(amount);
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException(nameof(amount));
+            }
+            Balance -= amount;
+            bonusPoints -= CalculateBanefitPoints(amount);
+        }
+
+        protected abstract bool IsValidBalance(decimal value);
+
+        protected abstract int CalculateBanefitPoints(decimal amount);
+    }
+}
