9a759fc1662b2d522ef7d8420ee5bf930bbd0e18
akatomakhina <katomakhinaanastasiya@gmail.com>
2018-04-01 22:00:13 +0300

    Project for Day9 added

---
+++ NET.S.2018.Katomakhina.09/M13.Streams.Task/ConsoleClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using static StreamsDemo.StreamsExtension;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var source = ConfigurationManager.AppSettings["sourceFilePath"];
+
+            var destination = ConfigurationManager.AppSettings["destinationFiePath"];
+
+            Console.WriteLine($"ByteCopy() done. Total bytes: {ByByteCopy(source, destination)}");
+
+            Console.WriteLine($"InMemoryByteCopy() done. Total bytes: {InMemoryByByteCopy(source, destination)}");
+
+            Console.WriteLine(IsContentEquals(source, destination));*/
+            var source = @"..\..\..\Resources\SourceText.txt";
+            var destination = @"..\..\..\Resources\DestinationText.txt";
+            Console.WriteLine(InMemoryByByteCopy(source, destination));
+            Console.ReadKey();
+            //etc
+        }
+    }
+}
