0098cffef99afd38f3bdd4af2333d963434a13a4
akatomakhina <katomakhinaanastasiya@gmail.com>
2018-04-08 16:06:51 +0300

    Sorting added

---
+++ NET.S.2018.Katomakhina.06/Sorting/AlgorithmSorting/BubbleSorting.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmSorting
+{
+    public class BubbleSorting
+    {
+        #region Public methods
+
+        public static void Sort(int[][] jaggedArray, IComparer<int[]> comparator) =>
+            BubbleSort(jaggedArray, comparator);
+
+        public static void Sort(int[][] jaggedArray, Comparison<int[]> comparator) =>
+            BubbleSort(jaggedArray, new DelegateForSort(comparator));
+
+        #endregion
+
+        #region Private methods
+
+        private static void BubbleSort(int[][] jaggedArray, IComparer<int[]> comparator)
+        {
+            Validation(jaggedArray, comparator);
+
+            for (int i = 0; i < jaggedArray.Length - 1; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - 1; j++)
+                {
+                    if (comparator.Compare(jaggedArray[j], jaggedArray[j + 1]) > 0)
+                    {
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                    }
+                }
+            }
+        }
+
+        private static void Validation(int[][] jaggedArray, IComparer<int[]> comparator)
+        {
+            if (ReferenceEquals(jaggedArray, null))
+            {
+                throw new ArgumentException(nameof(jaggedArray));
+            }
+
+            if (ReferenceEquals(comparator, null))
+            {
+                throw new ArgumentException(nameof(comparator));
+            }
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                if (ReferenceEquals(jaggedArray[i], null))
+                {
+                    throw new ArgumentException(nameof(jaggedArray), $"{i}-th nested array in {nameof(jaggedArray)} is null");
+                }
+            }
+        }
+
+        private static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        #endregion
+    }
+}
---
+++ NET.S.2018.Katomakhina.06/Sorting/AlgorithmSorting/DelegateForSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmSorting
+{
+    internal class DelegateForSort : IComparer<int[]>
+    {
+        private Comparison<int[]> delegateComparator;
+
+        public DelegateForSort(Comparison<int[]> comparator)
+        {
+            if (ReferenceEquals(comparator, null))
+            {
+                throw new ArgumentNullException(nameof(comparator));
+            }
+
+            delegateComparator = comparator;
+        }
+
+        public int Compare(int[] x, int[] y) =>
+            delegateComparator(x, y);
+    }
+}
