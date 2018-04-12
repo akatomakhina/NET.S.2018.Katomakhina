7f677db1f492b51327256d55b8090a94f89d6871
akatomakhina <katomakhinaanastasiya@gmail.com>
2018-04-12 17:17:58 +0300

    BinarySearch fixed

--- NET.S.2018.Katomakhina.14/BinarySearch/BinarySearchLogic/BinarySearcher.cs
+++ NET.S.2018.Katomakhina.14/BinarySearch/BinarySearchLogic/BinarySearcher.cs
@@ -1,62 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BinarySearchLogic
 {
     public class BinarySearcher
     {
-
-        public static int BinarySearch<T>(T[] arr, T value, Comparison<T> comparer)
+        public static int BinarySearch<T>(T[] array, T value, Comparison<T> comparer)
         {
-            if (ReferenceEquals(arr, null))
+            if (ReferenceEquals(array, null))
             {
-                throw new ArgumentNullException($"{nameof(arr)} is null");
+                throw new ArgumentNullException($"{nameof(array)} is null");
             }
 
-            if (arr.Length == 0)
+            if (array.Length == 0)
             {
                 return -1;
             }
 
-            if (ReferenceEquals(comparer, null))
-            {
-                comparer = DefaultComparison(arr);
-            }
-
-            int left = 0;
-            int right = arr.Length;
+            int first = 0;
+            int last = array.Length;
             int mid = 0;
 
-            while (left < right)
+            while (first < last)
             {
-                mid = left + (right - left) / 2;
+                mid = first + (last - first) / 2;
 
-                if (comparer(arr[mid], value) == 0)
+                if (comparer(array[mid], value) == 0)
                     return mid;
 
-                if (comparer(arr[mid], value) > 0)
-                    right = mid;
+                if (comparer(array[mid], value) > 0)
+                    last = mid;
                 else
-                    left = mid + 1;
+                    first = mid + 1;
             }
 
-            return -(1 + left);
-        }
-
-        private static Comparison<T> DefaultComparison<T>(T[] arr)
-        {
-            if (arr[0] is IComparable<T>)
-            {
-                return (x, y) => (x as IComparable<T>).CompareTo(y);
-            }
-            if (arr[0] is IComparable)
-            {
-                return (x, y) => (x as IComparable).CompareTo(y);
-            }
-            throw new ArgumentException("there is no comparer");
+            return -(1 + first);
         }
     }
 }
