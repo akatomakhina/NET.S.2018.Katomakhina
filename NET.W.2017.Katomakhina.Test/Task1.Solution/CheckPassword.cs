778b558061f8ff516aaec6a8544b1d877f4a86ce
akatomakhina <katomakhinaanastasiya@gmail.com>
2018-04-23 12:05:50 +0300

    Task1Solution with added

---
+++ NET.W.2017.Katomakhina.Test/Task1.Solution/CheckPassword.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Solution
+{
+    public class CheckPassword : IVerifyPassword
+    {
+        public bool VerifyPassword(string password)
+        {
+            bool flag = false;
+
+            if (password == null)
+            {
+                return flag;
+                throw new ArgumentException($"{nameof(password)} is null arg");
+            }                
+
+            if (password == string.Empty)
+            {
+                return flag;
+                throw new ArgumentException($"{nameof(password)} is empty ");                
+            }            
+
+            if (password.Length <= 7)
+            {
+                return flag;
+                throw new ArgumentOutOfRangeException($"{nameof(password)} length too short ");
+            }
+
+            if (password.Length >= 15)
+            {
+                return flag;
+                throw new ArgumentOutOfRangeException($"{nameof(password)} length too long ");
+            }
+
+            if (!password.Any(char.IsLetter))
+            {
+                return flag;
+                throw new ArgumentException($"{nameof(password)} hasn't alphanumerical chars ");
+            }
+
+            if (!password.Any(char.IsNumber))
+            {
+                return flag;
+                throw new ArgumentException($"{nameof(password)} hasn't digits ");
+            }
+
+            return flag = true;
+        }
+    }
+}
---
+++ NET.W.2017.Katomakhina.Test/Task1.Solution/PasswordCheckerService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Solution
+{
+    public class PasswordCheckerService
+    {
+        private SqlRepository repository = new SqlRepository();
+
+        IEnumerable<IVerifyPasswordTwo> verifiables;
+
+        public Tuple<bool, string> VerifyPassword(string password)
+        {
+            
+            foreach (var verifiable in verifiables)
+            {
+                var temp = verifiable.VerifyPassword(password);
+                if (temp.Item1)
+                {
+                    return Tuple.Create(false, temp.Item2);
+                }
+            }
+
+            repository.Create(password);
+
+            return Tuple.Create(true, "Password is Ok. User was created");
+        }
+    }
+}
